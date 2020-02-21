3c6fc61838e9b98ae480fd42551b7df4c337b5b8
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-02-21 09:41:00 +0200

    tttt

--- tehtävät/t4/t4.py
+++ tehtävät/t4/t4.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 #df = pd.read_csv('http://gpspekka.kapsi.fi/trafi56.zip', sep = ',', decimal='.')
 df = pd.read_csv('./testi/trafi56.zip', sep = ';', decimal=',', encoding='latin_1', nrows=10000)
+#df['time'] = %%time
 
 print(df)
