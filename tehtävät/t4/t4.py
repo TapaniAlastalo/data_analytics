67a51754d49c0beed2876987e8a5fc0cc18a6bbb
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-02-26 15:25:26 +0200

    t4 valmis

--- tehtävät/t4/t4.py
+++ tehtävät/t4/t4.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #df = pd.read_csv('http://gpspekka.kapsi.fi/trafi56.zip', sep = ',', decimal=',', encoding='latin_1', nrows=10000)
-df = pd.read_csv('../testi/trafi56.zip', sep = ';', decimal=',', usecols=['kayttoonottopvm', 'merkkiSelvakielinen', 'ensirekisterointipvm', 'sahkohybridi'], encoding='latin_1', nrows=100000)
+df = pd.read_csv('../testi/trafi56.zip', sep = ';', decimal=',', usecols=['kayttoonottopvm', 'merkkiSelvakielinen', 'ensirekisterointipvm', 'sahkohybridi'], encoding='latin_1', nrows=4000000)
 #df = pd.read_csv('../testi/trafi56.zip', sep = ';', decimal=',', usecols=['kayttoonottopvm', 'merkkiSelvakielinen', 'ensirekisterointipvm', 'sahkohybridi'], encoding='latin_1')
 #df['time'] = %%time
 
@@ -18,7 +18,8 @@ dft2 = pd.DataFrame(df2.groupby('merkkiSelvakielinen')['merkkiSelvakielinen'].co
 print(dft2.head(5))
 
 print('\ntee uusi sarake rekvuosi, johon luet 4 ensimmäistä merkkiä kentästä ensirekisterointipvm. Ristiintaulukoi sitten kentän sahkohybridi prosenttiosuudet (True/False) rekvuoden mukaan vuodesta 2001 eteenpäin.')
-df2['rekvuosi'] = None
-df2.loc[(df['ensirekisterointipvm'].notnull()), 'rekvuosi'] = df2['ensirekisterointipvm'].str[0:4]
-dft3v1 = pd.crosstab(df2['rekvuosi'],  df2['sahkohybridi'], normalize = 'index').applymap("{:.1%}".format)
-print(dft3v1)
+df2['rekvuosi'] = 0
+df2.loc[(df['ensirekisterointipvm'].notnull()), 'rekvuosi'] = df2['ensirekisterointipvm'].str[0:4].astype(float)
+dft3 = df2[(df2['rekvuosi'] > 2000)]
+dft3 = pd.crosstab(dft3['rekvuosi'],  dft3['sahkohybridi'], normalize = 'index').applymap("{:.1%}".format)
+print(dft3)
