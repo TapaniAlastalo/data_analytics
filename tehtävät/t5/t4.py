fc05966e6b8e96fcfbc8b37b0d9da8bc9a1cefec
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-03-05 01:19:41 +0200

    all clear

--- tehtävät/t5/t4.py
+++ tehtävät/t5/t4.py
@@ -14,16 +14,69 @@ def laskeMoneskoPv(x):
     return x['Pv'] + pv
 
 df['vuodenpv'] = df.apply(laskeMoneskoPv, axis=1)
-print(df)
+#print(df)
 
-df2 = df.groupby(['Vuosi','vuodenpv'])['Lumensyvyys (cm)'].max().unstack()
-print(df2['Vuosi'])
+df2 = df.groupby(['Vuosi','vuodenpv'])['Lumensyvyys (cm)', 'Ilman lämpötila (degC)'].max()#.unstack()
+print(df2)
+print(df2.unstack())
 
-fig, ax = plt.subplots()
+print('reset')
+df2 = df2.reset_index()
+print(df2)
 
-#plt.plot('Vuosi', 'Lumensyvyys (cm)', 'r-', label='jee', data = df2)
-#plt.plot('Lumensyvyys (cm)', data = df2)
-#plt.plot(df['Vuosi'],df.iloc[:,1:])
-#df.plot('Lumensyvyys (cm)',['2018', '2019', '2020'])
+df2018 = df2[df2['Vuosi'] == 2018]
+df2019 = df2[df2['Vuosi'] == 2019]
+df2020 = df2[df2['Vuosi'] == 2020]
+
+df2020['muutoseiliseen'] = df2020['Lumensyvyys (cm)'] - df2020['Lumensyvyys (cm)'].shift()
+df2020.fillna(0, inplace=True)
+df2020 = df2020.reset_index()
+df2019 = df2019.reset_index()
+df2020['erotus2019'] = df2020['Lumensyvyys (cm)'] - df2019['Lumensyvyys (cm)']
+df2020.fillna(0, inplace=True)
+
+#fig, (ax1, ax2, ax3, ax4) = plt.subplots() #(2, 2, figsize=(14, 14))
+#fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 1, sharex=True, sharey=True, figsize=(14, 14))
+
+#ax1.plot(df2018['vuodenpv'], df2018['Lumensyvyys (cm)'], 'b-', label='2018')
+#ax1.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
+#ax1.plot(df2020['vuodenpv'], df2020['Lumensyvyys (cm)'], 'g-', label='2020')
+#ax1.set_xlim(0, 60)
+
+#ax2.plot(df2018['vuodenpv'], df2018['Lumensyvyys (cm)'], 'b-', label='2018')
+#ax3.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
+#ax4.plot(df2020['vuodenpv'], df2020['Lumensyvyys (cm)'], 'g-', label='2020')
+
 #plt.legend(loc='upper left')
+
+plt.figure(figsize=(14, 14))  # luodaan uusi kuvio, tämä on "nykyinen kuvio".
+
+plt.subplot(2,2,1)  # tehdään nykyiseen kuvioon 2 riviä, 1 sarake -"ruudukko" ja otetaan 1. paikka "nykyiseksi kaavioksi"
+plt.plot(df2018['vuodenpv'], df2018['Lumensyvyys (cm)'], 'b-', label='2018')
+plt.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
+plt.plot(df2020['vuodenpv'], df2020['Lumensyvyys (cm)'], 'g-', label='2020')
+plt.legend(loc='upper left') # lisätään selite "nykyiseen kaavioon"
+plt.xlim(0, 60)
+
+plt.subplot(2,2,2)
+plt.plot(df2018['vuodenpv'], df2018['Lumensyvyys (cm)'], 'b-', label='2018')
+plt.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
+plt.plot(df2020['vuodenpv'], df2020['Lumensyvyys (cm)'], 'g-', label='2020')
+plt.xlim(0, 60)
+
+plt.subplot(2,2,3)
+#plt.bar(df2020['muutoseiliseen'], df2020['vuodenpv'])
+plt.hist(df2020['muutoseiliseen'], df2020['vuodenpv'])
+plt.xlim(-15, 15)
+#plt.ylim(0, 75)
+
+
+#plt.subplot(2,2,4)
+#plt.plot(df2018['vuodenpv'], df2018['Lumensyvyys (cm)'], 'b-', label='2018')
+#plt.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
+#plt.plot(df2020['vuodenpv'], df2020['Lumensyvyys (cm)'], 'g-', label='2020')
+#plt.xlim(0, 60)
+#plt.figure(2)
+#sns.regplot(df2020['Ilman lämpötila (degC)'], df2020['muutoseiliseen'])
+
 #plt.show()
