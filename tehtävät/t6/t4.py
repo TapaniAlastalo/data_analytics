bb612142b61de1d9166c64c42a0bea303bc4dc6f
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-03-23 12:58:22 +0200

    prepared t4 & t5

--- tehtävät/t6/t4.py
+++ tehtävät/t6/t4.py
@@ -1,5 +1,46 @@
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('http://student.labranet.jamk.fi/~varpe/datananal2k2020/kerta6/km.txt', sep=",", decimal='.')
+df = pd.read_csv('http://student.labranet.jamk.fi/~varpe/datananal2k2020/kerta6/km.txt', sep=",", decimal='.')#, index_col='pvm')
+
+#df['Vuosi'] = df['pvm'].str.split('-').str.get(0).str.strip().astype(int)
+#df['Kk'] = df['pvm'].str.split('-').str.get(1).str.strip().astype(int)
+#df['Pv'] = df['pvm'].str.split('-').str.get(2).str.strip().astype(int)
+df['y'], df['m'], df['d'] = df['pvm'].str.split('-', 2).str
+
+df['pvm']  = pd.to_datetime(df['pvm'])
+df.set_index('pvm', inplace=True)
+df.sort_index(ascending=True, inplace=True)
 print(df)
+
+print(df['2019'])
+print(df[(df['y']==2019)]['km'])
+#print(df['Kk'].sort_values(ascending=False).head(10))
+
+#plt.figure()  
+#ax1 = plt.subplot(2,2,1) 
+#df[df['2019']].plot(y='Lumensyvyys (cm)', label='2019', ax=ax1)
+#df[df['2018']].plot(y='Lumensyvyys (cm)', label='2018', ax=ax1)
+#df[df['2017']].plot(y='Lumensyvyys (cm)', label='2017', ax=ax1)
+#plt.legend()
+#plt.ylabel('lumensyvyydet tammikuussa')
+
+#fig = plt.gcf()  #antaa nykyisen kuvion (current figure)
+#fig.set_size_inches(14, 14)
+
+#plt.show()
+
+df['km'].plot()
+df['km'].rolling('7d').mean().plot(label='rollWeek')
+df['km'].rolling('365d').mean().plot(label='rollYear')
+plt.legend()
+
+plt.show()
+
+#df['km'].plot()
+#df['km'].rolling(20).mean.plot(label='1d')
+#df['km'].rolling(20, center=True).mean().plot(label='roll20c')
+#plt.legend()
+#plt.show()
--- tehtävät/t6/t5.py
+++ tehtävät/t6/t5.py
@@ -1,7 +1,39 @@
 import pandas as pd
 import datetime
+from dateutil.parser import parse
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('http://student.labranet.jamk.fi/~varpe/datananal2k2020/kerta6/accesslog.zip', sep=",", decimal='.')
+#print(df)
+#df['date'], df['h'], df['min'], df['s'] = df['timestamp'].str.split(':', 3).str
+#df['y'], df['m'], df['d'] = df['date'].str.split('/', 2).str
+df['dt'] = df['timestamp'].str.replace(':', ' ', 1)
 print(df)
+df['timestamp'] = df['dt'].map(lambda x: parse(x))
+print(df['timestamp'])
+#df['pvm']  = pd.to_datetime(df['timestamp'])
+#df  = pd.to.datetime(df['timestamp'], format='%d:%b/%Y:%H:%M:%S')
+#df.set_index('pvm', inplace=True)
+#df.sort_index(ascending=True, inplace=True)
+#print(df)
+df['timestamp']  = pd.to_datetime(df['timestamp'])
+df.set_index('timestamp', inplace=True)
+df.sort_index(ascending=True, inplace=True)
+print(df)
+
+
+
+df2 = df['2018-05-08 11:00:00':'2018-05-08 14:00:00']
+print(df2)
+
+#plt.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
+df['eventid'].count.plot()
+#df['km'].rolling('7d').mean().plot(label='rollWeek')
+#df['km'].rolling('365d').mean().plot(label='rollYear')
+plt.legend()
+
+plt.show()
+
 
 
