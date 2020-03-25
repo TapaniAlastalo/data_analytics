97ff6742024501b8bb836ff089d1ecad5afcc620
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-03-25 17:31:01 +0200

    t4 ja t5 sekoilua

--- tehtävät/t6/t4.py
+++ tehtävät/t6/t4.py
@@ -8,39 +8,51 @@ df = pd.read_csv('http://student.labranet.jamk.fi/~varpe/datananal2k2020/kerta6/
 #df['Vuosi'] = df['pvm'].str.split('-').str.get(0).str.strip().astype(int)
 #df['Kk'] = df['pvm'].str.split('-').str.get(1).str.strip().astype(int)
 #df['Pv'] = df['pvm'].str.split('-').str.get(2).str.strip().astype(int)
-df['y'], df['m'], df['d'] = df['pvm'].str.split('-', 2).str
+#df['end'] = df['pvm'].str.split('-').str.get(1).str.strip() + df['pvm'].str.split('-').str.get(2).str.strip()
+#df['y'], df['m'], df['d'] = df['pvm'].str.split('-', 2).str
 
 df['pvm']  = pd.to_datetime(df['pvm'])
 df.set_index('pvm', inplace=True)
 df.sort_index(ascending=True, inplace=True)
 print(df)
+#df['2019']
+df2019 = df['2019-05-01':'2019-10'].resample('1D').asfreq().fillna(0)#.ffill()
+df2018 = df['2018-05-01':'2018-10'].resample('1D').asfreq().fillna(0)
+df2017 = df['2017-05-01':'2017-10'].resample('1D').asfreq().fillna(0)
+df2016 = df['2016-05-01':'2016-10'].resample('1D').asfreq().fillna(0)
+df2015 = df['2015-05-01':'2015-10'].resample('1D').asfreq().fillna(0)
+#df2018.set_index('end', inplace = True)
+#df2019.set_index('end', inplace = True)
+df2019 = df2019.reset_index()
+df2018 = df2018.reset_index()
+df2017 = df2017.reset_index()
+df2016 = df2016.reset_index()
+df2015 = df2015.reset_index()
 
-print(df['2019'])
-print(df[(df['y']==2019)]['km'])
-#print(df['Kk'].sort_values(ascending=False).head(10))
+df2019['sum'] = df2019['km'].cumsum()
+df2018['sum'] = df2018['km'].cumsum()
+df2017['sum'] = df2017['km'].cumsum()
+df2016['sum'] = df2016['km'].cumsum()
+df2015['sum'] = df2015['km'].cumsum()
 
-#plt.figure()  
-#ax1 = plt.subplot(2,2,1) 
-#df[df['2019']].plot(y='Lumensyvyys (cm)', label='2019', ax=ax1)
-#df[df['2018']].plot(y='Lumensyvyys (cm)', label='2018', ax=ax1)
-#df[df['2017']].plot(y='Lumensyvyys (cm)', label='2017', ax=ax1)
-#plt.legend()
-#plt.ylabel('lumensyvyydet tammikuussa')
+print(df2018)
+print(df2019)
+#print(df2019b)
+print('test')
+df2018['erotus'] = df2018['sum'] - df2019['sum']
+df2017['erotus'] = df2017['sum'] - df2019['sum']
+df2016['erotus'] = df2016['sum'] - df2019['sum']
+df2015['erotus'] = df2015['sum'] - df2019['sum']
+print(df2018)
+#df2018roll = df2018['km'].rolling('1d').sum() - df2019['km'].rolling('1d').sum()
+#print(df2018roll)
+#df2018['km'].rolling(1).sum().plot(label='roll1')
+#df2019['km'].rolling(1, center=True).sum().plot(label='roll2')
+#(df2018['km'].rolling('1d').sum()).plot(label='roll3')
 
-#fig = plt.gcf()  #antaa nykyisen kuvion (current figure)
-#fig.set_size_inches(14, 14)
-
-#plt.show()
-
-df['km'].plot()
-df['km'].rolling('7d').mean().plot(label='rollWeek')
-df['km'].rolling('365d').mean().plot(label='rollYear')
+df2018['erotus'].plot(label='2018')
+df2016['erotus'].plot(label='2017')
+df2015['erotus'].plot(label='2016')
+df2015['erotus'].plot(label='2015')
 plt.legend()
-
 plt.show()
-
-#df['km'].plot()
-#df['km'].rolling(20).mean.plot(label='1d')
-#df['km'].rolling(20, center=True).mean().plot(label='roll20c')
-#plt.legend()
-#plt.show()
--- tehtävät/t6/t5.py
+++ tehtävät/t6/t5.py
@@ -4,7 +4,7 @@ from dateutil.parser import parse
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('http://student.labranet.jamk.fi/~varpe/datananal2k2020/kerta6/accesslog.zip', sep=",", decimal='.')
+df = pd.read_csv('http://student.labranet.jamk.fi/~varpe/datananal2k2020/kerta6/accesslog.zip', sep=",", decimal='.', nrows=10000)
 #print(df)
 #df['date'], df['h'], df['min'], df['s'] = df['timestamp'].str.split(':', 3).str
 #df['y'], df['m'], df['d'] = df['date'].str.split('/', 2).str
@@ -23,17 +23,33 @@ df.sort_index(ascending=True, inplace=True)
 print(df)
 
 
-
+print("--------df2----------")
 df2 = df['2018-05-08 11:00:00':'2018-05-08 14:00:00']
+#df2['time'] = 
 print(df2)
+df3 = df2.groupby(['timestamp','eventid'])['ip']#.nunique().sort_values(ascending=False)#.unstack().count()
+#df3.fillna(0, inplace=True)
+print(df3)
 
-#plt.plot(df2019['vuodenpv'], df2019['Lumensyvyys (cm)'], 'r-', label='2019')
-df['eventid'].count.plot()
-#df['km'].rolling('7d').mean().plot(label='rollWeek')
-#df['km'].rolling('365d').mean().plot(label='rollYear')
-plt.legend()
+#df22 = df2.resample('5min')
+#print(df22)
 
-plt.show()
+#event1 = df3[df3['eventid'] == 2018]
+#df4 = df3.unstack().fillna(0)
+#print(df4)
+#print(df3.unstack().unstack())
+#print('test')
+#print(df4.resample('5min').count())
+#print('testeest')
+#print(df4['20180508_MQ_MA'])
+#print(df4['20180508_MQ_MA'].resample('5min').count())
 
-
-
+#df4['20180508_MQ_MA'].rolling(300).count().plot()
+#df4['20180508_MQ_MA'].rolling('5min').mean().plot()
+#df4['20180508_MQ_MB'].rolling('5min').mean().plot()
+#df4['20180508_MQ_MC'].rolling('5min').sum().plot()
+#df4['20180508_MQ_WA'].rolling('5min').count().plot()
+#df4['20180508_MQ_WB'].rolling('5min').count().plot()
+#df4['20180508_MQ_WC'].rolling('5min').count().plot()
+#plt.legend()
+#plt.show()
